acac5b736c3e69a17667e56dcd5854901ad82b1d
Chris Hartjes <chartjes@littlehart.net>
2017-03-21 17:04:21 -0400

    Added in some tests that look at __version__ and __heartbeat__, checking for expected fields

---
+++ config-test/test_server_details.py
@@ -0,0 +1,44 @@
+import configparser
+import pytest
+import requests
+
+from pytest_testrail.plugin import testrail
+
+
+@pytest.fixture
+def conf():
+    config = configparser.ConfigParser()
+    config.read('manifest.ini')
+    return config
+
+
+def test_version(conf, env):
+    response = requests.get(conf.get(env, 'server') + '/__version__')
+    data = response.json()
+
+    expected_fields = {'version', 'source', 'name', 'build', 'commit'}
+
+    # First, make sure that data only contains fields we expect
+    for key in data:
+        assert key in expected_fields
+
+    # Then make the we only have the expected fields in the data
+    for field in expected_fields:
+        assert field in data
+
+
+def test_heartbeat(conf, env):
+    response = requests.get(conf.get(env, 'server') + '/__heartbeat__')
+    data = response.json()
+
+    expected_fields = {'permission', 'cache', 'storage'}
+
+    # First, make sure that data only contains fields we expect
+    for key in data:
+        assert key in expected_fields
+
+    # Then make the we only have the expected fields in the data
+    for field in expected_fields:
+        assert field in data
+
+
