d3e1da97ea038038b72b3bfaaeb586d81c7235e9
Chris Hartjes <chartjes@littlehart.net>
2017-04-25 11:32:25 -0400

    Trying to put some helper methods in their own module

---
+++ fxtesteng/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='fxtesteng',
+      url='https://mozilla.org',
+      author='Chris Hartjes',
+      author_email='chartjes@mozilla.com',
+      version='1.0',
+      py_modules=['helpers'])
