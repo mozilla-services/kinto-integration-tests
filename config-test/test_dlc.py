7eb2051f29752961f61ea256ff240265e214a4b6
Chris Hartjes <chartjes@littlehart.net>
2017-07-26 15:23:44 -0400

    Fixed tests to use CDN endpoints instead of S3 buckets, removed unneeded library import

--- config-test/test_dlc.py
+++ config-test/test_dlc.py
@@ -4,9 +4,6 @@ import pytest
 import requests
 
 
-from kinto_http import Client, KintoException
-
-
 @pytest.fixture
 def conf():
     config = configparser.ConfigParser()
@@ -22,14 +19,14 @@ def test_fennec_dlc_exists(env, conf):
         # Make sure it's a type of record we're expecting
         assert 'kind' in record or 'hyphenation' in record
 
-        # Make sure the file exists
-        response = requests.get(conf.get(env, 's3_location') + record['attachment']['location'])
-        assert response.status_code != 404
+        # Make sure the file existsZ
+        url = "%s/%s" % (conf.get(env, 'cdn'), record['attachment']['location'])
+        response = requests.get(url)
+        assert int(response.status_code) != 404
         assert len(response.content) > 0
 
         # Check the file hash matches
         m = hashlib.sha256()
         m.update(response.content)
         assert record['attachment']['hash'] == m.hexdigest()
-
 
