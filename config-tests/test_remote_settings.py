8db5bf9dafea077c195d294f7bc833f77a604a22
Chris Hartjes <chartjes@littlehart.net>
2018-05-09 16:06:22 -0400

    Test now uses a random collection ID instead of 'qatest', fixed bug with not using the correct value for the reviewer when doing a group patch, removed unneeded test

--- config-tests/test_remote_settings.py
+++ config-tests/test_remote_settings.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import uuid
 
 from kinto_http import Client
 from kinto_http.patch_type import JSONPatch
@@ -13,7 +14,7 @@ from kinto_http.patch_type import JSONPatch
 def test_create_collection_in_main_bucket(conf, env):
     # Create a record inside the 'main-workspace' bucket and then
     # PATCH the collection to say someone else can review it
-    collection = "qatest"
+    collection = uuid.uuid4()
     client = Client(
         server_url=conf.get(env, "writer_server"),
         auth=(os.getenv("RS_USER_1_LOGIN"), os.getenv("RS_USER_1_PASSWD")),
@@ -21,26 +22,28 @@ def test_create_collection_in_main_bucket(conf, env):
     )
     client.create_collection(id=collection, if_not_exists=True)
     changes = JSONPatch(
-        [dict(op="add", path="/data/members/0", value=os.getenv("RS_USER_2_LOGIN"))]
+        [dict(op="add", path="/data/members/0", value="ldap:{0}".format(os.getenv("RS_USER_2_LOGIN")))]
     )
     client.patch_group(
         id="{0}-reviewers".format(collection),
         changes=changes
     )
+
     data = {"qa": "testing"}
     cr_response = client.create_record(data=data, collection=collection)
     new_record_id = cr_response["data"]["id"]
     assert new_record_id is not False
 
-    # Now request a review of these changes
+    # Now request a review of these changes and verify the record ends up in the
+    # 'main-preview' bucket
     client.patch_collection(
         id=collection,
         data={"status": "to-review"}
     )
-    pre_sign_info = client.get_collection(id=collection)
     assert client.get_record(id=new_record_id, collection=collection, bucket='main-preview') is not False
 
     # Create a second client to act as the reviewer and approve the request
+    # and verify that the record is now part of the 'main' bucket
     reviewer_client = Client(
         server_url=conf.get(env, "writer_server"),
         auth=(os.getenv("RS_USER_2_LOGIN"), os.getenv("RS_USER_2_PASSWD")),
@@ -51,10 +54,6 @@ def test_create_collection_in_main_bucket(conf, env):
         data={"status": "to-sign"}
     )
     assert client.get_record(id=new_record_id, collection=collection, bucket='main') is not False
-
-    # If last_signature_date has changed, we know the signing successfully happened
-    post_sign_info = client.get_collection(id=collection)
-    assert pre_sign_info["data"]["last_signature_date"] != post_sign_info["data"]["last_signature_date"]
 
     # Clean up after ourselves
     dr_response = client.delete_record(id=new_record_id, collection=collection)
