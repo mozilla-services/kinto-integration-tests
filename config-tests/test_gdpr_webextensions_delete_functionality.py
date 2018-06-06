0a2a7aa806777e0c0a31881c9ce2f930fd783635
Chris Hartjes <chartjes@littlehart.net>
2018-06-06 13:53:10 -0400

    Moved functionality related to Notes to a separate test

--- config-tests/test_gdpr_webextensions_delete_functionality.py
+++ config-tests/test_gdpr_webextensions_delete_functionality.py
@@ -1,5 +1,4 @@
 import configparser
-import requests
 import pytest
 import time
 import uuid
@@ -7,7 +6,7 @@ from fxa.__main__ import DEFAULT_CLIENT_ID
 from fxa.core import Client as FxaClient
 from fxa.plugins.requests import FxABearerTokenAuth
 from fxa.tests.utils import TestEmailAccount
-from kinto_http import Client
+from kinto_http import Client, KintoException
 
 
 @pytest.fixture
@@ -19,12 +18,11 @@ def conf():
 
 @pytest.mark.webextensions
 def test_delete_request_removes_data(conf, env):
-    pytest.skip('skipping until GDPR SQS listeners are deployed to production')
     # Create a test user on FxA
     acct = TestEmailAccount()
     email = acct.email
     passwd = str(uuid.uuid4())
-    fxaclient = FxaClient("https://api.accounts.firefox.com")
+    fxaclient = FxaClient(conf.get(env, 'account_server_api'))
     session = fxaclient.create_account(email, passwd)
     m = acct.wait_for_email(lambda m: "x-verify-code" in m["headers"])
 
@@ -35,9 +33,9 @@ def test_delete_request_removes_data(conf, env):
     auth = FxABearerTokenAuth(
         email,
         passwd,
-        scopes=['sync:addon_storage', 'https://identity.mozilla.com/apps/notes'],
+        scopes=['sync:addon_storage'],
         client_id=DEFAULT_CLIENT_ID,
-        account_server_url=conf.get(env, 'account_server_url'),
+        account_server_url=conf.get(env, 'account_server_api'),
         oauth_server_url=conf.get(env, 'oauth_server_url'),
     )
 
@@ -56,22 +54,7 @@ def test_delete_request_removes_data(conf, env):
     we_updated_records = we_client.get_records(collection=conf.get(env, 'qa_collection'), bucket='default')
     assert len(we_updated_records) == len(we_existing_records) + 1
 
-    # Add data to Notes (kintotp)
-    tp_client = Client(server_url=conf.get(env, 'tp_server_url'), auth=auth)
-    tp_existing_records = tp_client.get_records(collection='notes', bucket='default')
-    assert len(tp_existing_records) == 0
-    data = {"subject": "QA Test", "value": "This stuff should get deleted"}
-    tp_record = tp_client.create_record(
-        data=data,
-        collection='notes',
-        bucket='default',
-        permissions={"read": ["system.Everyone"]}
-    )
-    tp_record_id = tp_record['data']['id']
-    tp_updated_records = tp_client.get_records(collection='notes', bucket='default')
-    assert len(tp_updated_records) == len(tp_existing_records) + 1
-
-    # Get the aliases of the buckets we are putting data in and
+    # Get the aliases of the bucket we are putting data in and
     # make sure that an unauthenticated user can see these records
     # before we delete the account
     we_bucket_id = we_client.server_info()["user"]["bucket"]
@@ -81,26 +64,16 @@ def test_delete_request_removes_data(conf, env):
         bucket=we_bucket_id,
         collection=conf.get(env, 'qa_collection'))
     assert resp['data']['id'] == we_record_id
-    tp_bucket_id = tp_client.server_info()["user"]["bucket"]
-    anon_tp_client = Client(server_url=conf.get(env, 'tp_server_url'))
-    resp = anon_tp_client.get_record(
-        id=tp_record_id,
-        bucket=tp_bucket_id,
-        collection='notes')
-    assert resp['data']['id'] == tp_record_id
 
     # Delete FxA account
     acct.clear()
     fxaclient.destroy_account(email, passwd)
 
-    # Wait 5 minutes and then make sure the records do not exist
-    time.sleep(301)
-    resp = requests.get(
-        conf.get(env, 'we_server_url') +
-        '/buckets/{0}/collections/{1}/records/{2}'.format(we_bucket_id, conf.get(env, 'qa_collection'), we_record_id))
-    assert resp.status_code > 400
-    tp_bucket_id = tp_client.server_info()["user"]["bucket"]
-    resp = requests.get(
-        conf.get(env, 'tp_server_url') +
-        '/buckets/{0}/collections/notes/records/{1}'.format(tp_bucket_id, tp_record_id))
-    assert resp.status_code > 400
+    # Wait 1 minute and then make sure the records do not exist because the
+    # Kinto client will throw an exception for non-existent records
+    time.sleep(60)
+    with pytest.raises(KintoException):
+        resp = anon_we_client.get_record(
+            id=we_record_id,
+            bucket=we_bucket_id,
+            collection=conf.get(env, 'qa_collection'))
